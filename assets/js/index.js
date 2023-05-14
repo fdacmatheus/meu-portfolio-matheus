ef2b1cecbffa08228df57b6d33194d960383b92c
Matheus Ferreira <86373169+fdacmatheus@users.noreply.github.com>
2023-05-14 01:17:12 -0300

    Glass effect adicionado e animação do botão corrigida

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,23 @@ imagem.addEventListener('mouseover', () => {
 imagem.addEventListener('mouseout', () => {
   imagem.src = './assets/img/foto-matheus.png';
 });
+const arrow = document.querySelector(".seta");
+const botao = document.getElementById("botao");
+
+botao.addEventListener("mouseover", () => {
+  arrow.classList.toggle("active");
+});
+
+botao.addEventListener("mouseout", () => {
+  arrow.classList.toggle("active");
+});
+
+botao.addEventListener("click", () => {
+  arrow.classList.add("active");
+  setTimeout(() => {
+    arrow.classList.remove("active");
+  }, 1000);
+});
+
+
+  
